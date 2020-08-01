4b143c00af6a47de38b98d230cb8b4d0ef58d5e0
Monique.Bass01 <monique.bass01@gmail.com>
2020-08-01 10:08:35 -0500

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a comma seperated list of numbers.");
+            var myanswer = Console.ReadLine();
+
+            string [] finalanswer = myanswer.Split(",");
+            int finalproduct = 1;
+
+            foreach (var answer in finalanswer)
+            {
+                finalproduct *= int.Parse(answer);
+            }
+            Console.WriteLine("If I multiply these numbers together, my final answer is:");
+            Console.WriteLine(finalproduct);
+        }
+    }
+}
